b07e36d580ecd603a704ac7bc1c328323828f6e1
UG HONGSEOHYEON <127842244+xyz987164@users.noreply.github.com>
2025-04-11 13:59:29 +0900

    the first files

---
+++ moyora/src/main/java/com/project/moyora/MoyoraApplication.java
@@ -0,0 +1,13 @@
+package com.project.moyora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoyoraApplication.class, args);
+	}
+
+}
---
+++ moyora/src/test/java/com/project/moyora/MoyoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.moyora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
